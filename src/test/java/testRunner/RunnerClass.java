19a024a01f00d6b4e5d1b15bd7d6e815e088293d
ShivaShankaran <shivashankaran.kanniyappan@expleogroup.com>
2025-01-16 11:44:11 +0530

    changed to Junit in testRunner

--- src/test/java/testRunner/RunnerClass.java
+++ src/test/java/testRunner/RunnerClass.java
@@ -1,21 +1,19 @@
 package testRunner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
-				features = "src/test/resources",
-				glue = {"stepDefinition", "hooks"}, 
-				snippets = SnippetType.CAMELCASE, 
-				plugin = {"pretty", "json:target\\cucumber.json", 
-									"html:target\\htmlreport.html"},
-				tags = "@All_Scenario",
-				dryRun = true
+				features = "src/test/resources", 
+				glue = { "stepDefinition", "hooks" }, 
+				plugin = { "pretty", "json:target\\cucumber.json", 
+							"html:target\\htmlreport.html" }, 
+				tags = "@All_Scenario", 
+				dryRun = false		
 				)
-public class RunnerClass extends AbstractTestNGCucumberTests
-{
-		
-	 
-	
+public class RunnerClass {
+
 }
