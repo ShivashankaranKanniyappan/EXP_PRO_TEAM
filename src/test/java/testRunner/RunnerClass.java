c5de745b9ad3060a03bf46590c1644b888bff60e
Suthirshana Muniyappan <suthirshana.muniyappan@expleogroup.com>
2025-01-21 17:05:43 +0530

    Updated TestRunner class

--- src/test/java/testRunner/RunnerClass.java
+++ src/test/java/testRunner/RunnerClass.java
@@ -4,12 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-				features = "src/test/resources//feature05_ForgetPassword.feature", 
+				features = "src/test/resources", 
 				glue = { "stepDefinition", "hooks" }, 
 				plugin = { "pretty", "json:target\\cucumber.json", 
 							"html:target\\htmlreport.html" }, 
-				// tags = "@All_Scenarios", 
-				dryRun = false		
+				tags = "@All_Scenarios", 
+				dryRun = true		
 				)
 public class RunnerClass extends AbstractTestNGCucumberTests {
 
